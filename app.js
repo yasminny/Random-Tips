baa0103089c9ed859a6d662b9e66b47f7db10607
YASMIN NYGATE <YASMIN.NYGATE@GMAIL.COM>
2017-07-25 15:46:40 +0300

    refactor and more functionality added

--- app.js
+++ app.js
@@ -1,65 +1,141 @@
 let tipArray;
+const main = document.querySelector('main');
+
+/**
+ * --------------------VIew (UI manipulation)-------------------
+ */
+
+function initView() {
+  const baseMainTemplate = `<h1>Hello World</h1>
+<div class="img-bcg">
+<div class="hello">
+</div>
+</div>
+    <p>Add tips:</p>
+    <form id="tip-form">
+      <input placeholder="Add here" required>
+      <button class="add-tip-btn">Submit</button>
+    </form>`;
+  main.innerHTML = baseMainTemplate;
+  const formElem = document.getElementById('tip-form');
+  formElem.addEventListener('submit', handelNewTip);
+  main.querySelector('.hello').addEventListener('click', showTips);
+}
 
 function initTips() {
-  const displayTip = localStorage.getItem('hideTips') ? false : true;
-  const tipTextElement = document.getElementById('tip-text');
-  const selectedTip = tipArray[Math.floor(Math.random() * tipArray.length)];
+  const displayTip = localStorage.getItem('hideTips') !== 'true' ? true : false;
   const chances = Math.round(Math.random() * 100);
-  const tipBoxElem = document.querySelector('.tip-box');
-  const closeBtn = document.getElementById('close');
-  const nextBtn = document.getElementById('next');
-  const noMoreTipsBtn = document.getElementById('no-more');
-  const tipIndex = tipArray.findIndex((item) => item === selectedTip);
-
-  tipTextElement.setAttribute('tip-index', tipIndex);
-  closeBtn.addEventListener('click', closeTipDisplay);
-  nextBtn.addEventListener('click', nextTipDisplay);
-  noMoreTipsBtn.addEventListener('click', noMoreTipDisplay);
-  tipTextElement.innerText = selectedTip;
 
   if (displayTip && chances % 2 === 0) {
-    tipBoxElem.classList.remove('hide');
+    console.log('init tips');
+    showTips();
   }
 }
 
-function closeTipDisplay() {
-  const tipBoxElem = document.querySelector('.tip-box');
-  return tipBoxElem.classList.add('hide');
+function showTips() {
+  console.log('show tips func');
+
+  const tipTemplate = `<section>
+      <div class="tip-box">
+        <button type="button" id="close">X close</button>
+        <h2>Tip Of The Day</h2>
+        <p id="tip-text"></p>
+        <div class="buttons">
+          <button type="button" id="next">Next Tip</button>
+          <button type="button" id="no-more">Don't Show Again</button>
+        </div>
+      </div>
+    </section>`;
+  const selectedTip = tipArray[Math.floor(Math.random() * tipArray.length)];
+  const tipIndex = tipArray.findIndex((item) => item === selectedTip);
+  main.innerHTML += tipTemplate;
+
+  const tipTextElement = document.getElementById('tip-text');
+  const closeBtn = document.getElementById('close');
+  const nextBtn = document.getElementById('next');
+  const noMoreTipsBtn = document.getElementById('no-more');
+
+  tipTextElement.setAttribute('tip-index', tipIndex);
+  closeBtn.addEventListener('click', initView);
+  nextBtn.addEventListener('click', nextTipDisplay);
+  noMoreTipsBtn.addEventListener('click', noMoreTipDisplay);
+  tipTextElement.innerText = selectedTip;
 }
 
 function noMoreTipDisplay() {
-  localStorage.setItem('hideTips', true);
-  closeTipDisplay();
+  localStorage.setItem('hideTips', 'true');
+  initView()
 }
 
 function nextTipDisplay() {
   const tipTextElement = document.getElementById('tip-text');
   const oldTipIndex = parseInt(tipTextElement.getAttribute('tip-index'));
-  let nextTip;
-  if (oldTipIndex === tipArray.length - 1) {
-    nextTip = tipArray[0];
-  }
-  else {
-    nextTip = tipArray[oldTipIndex + 1];
-  }
-  const tipIndex = tipArray.findIndex((item) => item === nextTip);
-  tipTextElement.setAttribute('tip-index', tipIndex);
-  tipTextElement.innerText = nextTip;
+  const nextTip = (oldTipIndex + 1) % tipArray.length;
+  tipTextElement.setAttribute('tip-index', nextTip);
+  tipTextElement.innerText = tipArray[nextTip];
 }
 
+initView();
+
+/**
+ * ---------------------Model (data manipulation)-------------------------
+ */
+
 function getTipsData() {
+  if(localStorage.getItem('tipArray')){
+    tipArray = JSON.parse(localStorage.getItem('tipArray'));
+   return initTips();
+  }
   const xhr = new XMLHttpRequest();
   xhr.addEventListener('load', xhrLoadHandler);
+  xhr.addEventListener('error', xhrErrorHandler);
+  xhr.addEventListener('timeout', xhrTimeoutHandler);
+
 
   xhr.open('GET', 'tips.json');
   xhr.send();
 }
 
-function xhrLoadHandler(event) {
-  const myXhr = event.target;
+function setXhrInfoDisplay() {
+  const infoTemplate = `<section>
+      <div class="tip-box">
+        <button type="button" id="close">X close</button>
+        <h2></h2>
+      </div>
+    </section>`;
+  main.innerHTML += infoTemplate;
+  const closeBtn = document.getElementById('close');
+  closeBtn.addEventListener('click', initView);
+}
 
-  tipArray = JSON.parse(myXhr.response);
+function xhrErrorHandler() {
+  setXhrInfoDisplay();
+  const infoMsgElem = document.querySelector('.tip-box').querySelector('h2');
+  infoMsgElem.innerText = "Sadly, the tip of the day could not load";
+}
+
+function xhrTimeoutHandler() {
+  setXhrInfoDisplay();
+  const infoMsgElem = document.querySelector('.tip-box').querySelector('h2');
+  infoMsgElem.innerText = "The server request to get the tips of the day was timed out";
+}
+
+function xhrLoadHandler(e) {
+  const myXhr = e.target;
+
+  tipArray = tipArray !== undefined? tipArray.push(JSON.parse(myXhr.response)) : JSON.parse(myXhr.response);
+  localStorage.setItem('tipArray', JSON.stringify(tipArray));
   initTips();
+}
+
+function handelNewTip(e) {
+  e.preventDefault();
+  const target = e.target;
+  const inputElem = target.querySelector('input');
+  const newTip = inputElem.value;
+  tipArray.push(newTip);
+  localStorage.setItem('tipArray', JSON.stringify(tipArray));
+  inputElem.value = '';
 }
 
 getTipsData();
